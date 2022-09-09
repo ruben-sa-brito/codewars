90ec962d9cb5595c8c7455ad6b8fb51bab899213
Timewound <rubenjms1@gmail.com>
2022-09-08 23:24:29 -0300

    7 kyu

--- next_perfect_square.py
+++ next_perfect_square.py
@@ -1,10 +1,8 @@
+#https://www.codewars.com/kata/56269eb78ad2e4ced1000013
+
 from math import sqrt
 def find_next_square(sq):
     if sqrt(sq) - int(sqrt(sq)) !=0:
         return -1
     else:
         return (sqrt(sq)+1)**2
-
-
-
-
