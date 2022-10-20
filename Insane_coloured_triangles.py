aac7d05637582d50a64d13b14f65d0764cc13946
Timewound <rubenjms1@gmail.com>
2022-10-20 19:57:29 -0300

    closest i could

--- Insane_coloured_triangles.py
+++ Insane_coloured_triangles.py
@@ -1,50 +1,27 @@
 #https://www.codewars.com/kata/5a331ea7ee1aae8f24000175
+
+COLORS = set("RGB")
+
+
+def triangle_simple(row):
+    while len(row)>1:
+        row = ''.join( a if a==b else (COLORS-{a,b}).pop() for a,b in zip(row, row[1:]))
+    return row
+
 def triangle(row):
-    listc = ['R','G','B']
+    if len(row)<10:
+        return triangle_simple(row)
     
-        
+    x= 1
+    
+    while x !=0:
+        row = ''.join( a if a==b else (COLORS-{a,b}).pop() for a,b in zip(row, row[9:]))
+        if len(row)<= 10:
+            x = 0
+            
     
     if len(row) == 1:
-        return row
-    if len(row) % 2 == 0:
-        if row[0]==row[-1]:
-            return row[0]
-        listc.remove(row[-1])
-        listc.remove(row[0])
-        return listc[0]
+        return row    
     else:
-        
-        listi = list()
-        if row[0] == row[-2]:
-            listi.append(row[0])
-        else:
-            listc.remove(row[-2])
-            listc.remove(row[0])
-            listi.append(listc[0])
-            listc = ['R','G','B']
-        
-        if row[1] == row[-1]:
-            listi.append(row[1])
-        else:
-            listc.remove(row[-1])
-            listc.remove(row[1])
-            listi.append(listc[0])
-            listc = ['R','G','B']
-            
-        if listi[0] == listi[1]:
-            return listi[0] 
-        else:
-            listc.remove(listi[0])
-            listc.remove(listi[1])
-            
-            return listc[0]
-            
-print(triangle('RBRGBRBG'))            
-                  
-            
-            
-                
-        
-        
-
-     
+        return triangle_simple(row)
+                       
