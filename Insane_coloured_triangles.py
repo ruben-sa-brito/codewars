ec52aa315a27e073c3d031b50ef1a3823a75e9b3
Timewound <rubenjms1@gmail.com>
2022-10-19 21:33:22 -0300

    adicionando link

--- Insane_coloured_triangles.py
+++ Insane_coloured_triangles.py
@@ -1,30 +1,30 @@
-# def triangle(row):
-#     strc = str()
-#     colors = ['B','G','R']
-#     ctr = 1
-#     while True:
-#         if len(row) == 1:
-#             return row
-#         for a in row: 
+#https://www.codewars.com/kata/5a331ea7ee1aae8f24000175
+def triangle(row):
+    strc = str()
+    colors = ['B','G','R']
+    ctr = 1
+    while True:
+        if len(row) == 1:
+            return row
+        for a in row: 
             
-#             if ctr != 1:
-#                 if a == b:
-#                     strc += a
-#                 else:    
-#                     if ctr != 1:
-#                         colors.remove(a)
-#                         colors.remove(b)
-#                         strc += colors[0]
-#                         colors = ['B', 'G', 'R']
-#             b = a            
+            if ctr != 1:
+                if a == b:
+                    strc += a
+                else:    
+                    if ctr != 1:
+                        colors.remove(a)
+                        colors.remove(b)
+                        strc += colors[0]
+                        colors = ['B', 'G', 'R']
+            b = a            
             
-#             ctr = 0            
-#         ctr = 1
-#         row = strc
-#         if len(strc) > 1:
-#             strc = str()
-#         else:
-#             return strc
+            ctr = 0            
+        ctr = 1
+        row = strc
+        if len(strc) > 1:
+            strc = str()
+        else:
+            return strc
 
-a = [1,2,3,4,5] 
-print (a[1:])      
+     
