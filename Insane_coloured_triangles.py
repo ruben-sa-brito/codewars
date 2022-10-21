1e3a1226111c4ab394ec371dddcae31f89bc169e
Timewound <rubenjms1@gmail.com>
2022-10-20 22:36:43 -0300

    2 kyu - primeira solução funcional

--- Insane_coloured_triangles.py
+++ Insane_coloured_triangles.py
@@ -2,38 +2,61 @@
 color = set("RGB")
 
 
-def triangle_100(row):
+def triangle_82(row):
     x=1
     while x !=0:
-        row = ''.join( a if a==b else (color-{a,b}).pop() for a,b in zip(row, row[10:]))
-        if len(row)<= 10:
+        row = ''.join( a if a==b else (color-{a,b}).pop() for a,b in zip(row, row[9:]))
+        if len(row)< 10:
             x = 0
     if len(row) == 1:
         return row    
     else:
         return triangle_simple(row)
 
-def triangle_1000(row):
+def triangle_730(row):
     x=1
     while x !=0:
-            row = ''.join( a if a==b else (color-{a,b}).pop() for a,b in zip(row, row[99:]))
-            if len(row)<= 100:
+            row = ''.join( a if a==b else (color-{a,b}).pop() for a,b in zip(row, row[81:]))
+            if len(row)< 82:
                 x = 0
     if len(row) == 1:
-        return row    
+        return row
+    elif len(row) < 10:
+        return triangle_simple(row)    
     else:
-        return triangle_100(row)
+        return triangle_82(row)
 
-def triangle_10000(row):
+def triangle_6562(row):
     x=1
     while x !=0:
-            row = ''.join( a if a==b else (color-{a,b}).pop() for a,b in zip(row, row[999:]))
-            if len(row)<= 1000:
+            row = ''.join( a if a==b else (color-{a,b}).pop() for a,b in zip(row, row[729:]))
+            if len(row)< 730:
                 x = 0
     if len(row) == 1:
-        return row    
+        return row
+    elif len(row) < 10:
+        return triangle_simple(row) 
+    elif len(row)< 82:
+        return triangle_82(row)    
     else:
-        return triangle_1000(row)             
+        return triangle_730(row)
+
+def triangle_19684(row):
+    x=1
+    while x !=0:
+            row = ''.join( a if a==b else (color-{a,b}).pop() for a,b in zip(row, row[6561:]))
+            if len(row)< 6562:
+                x = 0
+    if len(row) == 1:
+        return row
+    elif len(row) < 10:
+        return triangle_simple(row) 
+    elif len(row)< 82:
+        return triangle_82(row)
+    elif len(row)< 730:
+        return triangle_730(row)    
+    else:
+        return triangle_6562(row)                  
 
 def triangle_simple(row):
     while len(row)>1:
@@ -41,12 +64,21 @@ def triangle_simple(row):
     return row
 
 def triangle(row):
-    if len(row)<100:
-        return triangle_100(row)
+    print(len(row))
+    if len(row)<10:
+        return triangle_simple(row)
     
-    if len(row)<1000:
-        return triangle_1000(row)
+    if len(row)<82:
+        return triangle_82(row)
+    
+    if len(row)<730:
+        return triangle_730(row)
     
     if len(row)<10000:
-        return triangle_10000(row) 
-                       
+        return triangle_6562(row)
+    
+    if len(row)<100000:
+        return triangle_19684(row) 
+
+
+                      
