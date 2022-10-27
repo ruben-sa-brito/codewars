623fe5ce8fe965919057030804c14634a1483c67
Timewound <rubenjms1@gmail.com>
2022-10-27 19:25:01 -0300

    6 kyu

--- onearray.py
+++ onearray.py
@@ -1,3 +1,4 @@
+#https://www.codewars.com/kata/5514e5b77e6b2f38e0000ca9
 def up_array(arr):
     
     for a in arr:
@@ -7,14 +8,9 @@ def up_array(arr):
         if arr == []:
             return None    
         
-
     ls = [int(x) for x in str(int(''.join(map(str, arr))) + 1)]
     if len(arr) > len(ls):
         for a in range(len(arr) - len(ls)):
             ls.insert(0,0)
     
-    return ls  
-
-
-
-print(up_array([]))     
+    return ls   
