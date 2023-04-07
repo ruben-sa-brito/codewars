9d9112ff40e99867f39d7de06d436b627aa8a01e
Timewound <rubenjms1@gmail.com>
2023-04-07 10:06:22 -0300

    add new kata, 6 kyu

---
+++ greatest_position.py
@@ -0,0 +1,16 @@
+#https://www.codewars.com/kata/5442e4fc7fc447653a0000d5
+def greatest_distance(arr):
+    distance = 0
+    index1 = 0
+    for num in arr:
+        index2 = 0
+        for num2 in arr:
+            if num == num2:
+                
+                dst = abs(index1 - index2)
+                if dst > 0 and dst > distance:
+                    distance = dst
+            index2 += 1        
+        index1 += 1            
+    
+    return distance               
