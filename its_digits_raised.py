37136e80393c1957efd1a63be7985e68c3219a0d
Timewound <rubenjms1@gmail.com>
2023-05-28 14:32:09 -0300

    add new kata, kyu 6

---
+++ its_digits_raised.py
@@ -0,0 +1,20 @@
+#https://www.codewars.com/kata/5626b561280a42ecc50000d1
+
+def sum_dig_pow(a, b): 
+    
+    numbers = [x+a for x in range(b-a+1)]
+
+    perfect_numbers = list()
+    
+    for num in numbers:
+        sum_num = 0
+        
+        for ind, digit in enumerate(str(num)):
+            
+            sum_num += int(digit) ** (ind+1)
+            
+        perfect_numbers.append(sum_num) if sum_num == num else perfect_numbers 
+            
+
+    return perfect_numbers
+
