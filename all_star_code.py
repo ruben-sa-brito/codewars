652e7f0919284fed5685a2835a1de2ee6ce02c45
Timewound <rubenjms1@gmail.com>
2023-06-11 13:51:09 -0300

    link

--- all_star_code.py
+++ all_star_code.py
@@ -1,3 +1,4 @@
+#https://www.codewars.com/kata/586560a639c5ab3a260000f3
 def rotate_2(str_, words):
 
     if len(words) == len(str_):
@@ -15,6 +16,3 @@ def rotate(str_):
     rotate_2(str_, rotate_list)
     
     return rotate_list
-
-
-print(rotate('abcde'))  
