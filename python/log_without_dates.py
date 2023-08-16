a08134c2cfe77f39e59d82820b16301bcde0d9f7
Timewound <rubenjms1@gmail.com>
2023-08-16 10:38:20 -0300

    7 kyu

---
+++ python/log_without_dates.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+
+def check_logs(log):
+    dias = 0
+    try:
+        hora_ant = datetime.strptime(log[0], '%H:%M:%S')
+    except:
+        return 0    
+    for hour in log:
+        
+        hora = datetime.strptime(hour, '%H:%M:%S')
+        if hora <= hora_ant:
+            dias +=1
+        hora_ant = hora
+                
+        
+    return dias
