52b5d85f65ebc2b9d4776500bf4e5a9aa1cce734
Timewound <rubenjms1@gmail.com>
2023-08-30 09:13:25 -0300

    7 kyu

---
+++ python/html_gen.py
@@ -0,0 +1,33 @@
+class HTMLGen:
+    
+    def a(self, in_html):
+        
+        return '<a>'+in_html+'</a>'
+    
+    def b(self, in_html):
+        
+        return '<b>'+in_html+'</b>'
+    
+    def p(self, in_html):
+        
+        return '<p>'+in_html+'</p>'
+    
+    def body(self, in_html):
+        
+        return '<body>'+in_html+'</body>'
+    
+    def div(self, in_html):
+        
+        return '<div>'+in_html+'</div>'
+    
+    def span(self, in_html):
+        
+        return '<span>'+in_html+'</span>'
+    
+    def title(self, in_html):
+        
+        return '<title>'+in_html+'</title>'
+    
+    def comment(self, in_html):
+        
+        return '<!--'+in_html+'-->'      
