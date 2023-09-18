45e4a1741a44d42f3f01cf633c2e2908fc2442c2
Timewound <rubenjms1@gmail.com>
2023-09-18 11:06:46 -0300

    6 kyu

--- python/multiples_of_3_or_5.py
+++ python/multiples_of_3_or_5.py
@@ -1,3 +1,3 @@
 #https://www.codewars.com/kata/514b92a657cdc65150000006
 def solution(number):
-    return sum([x for x in range(number) if (x+1) % 3  == 0 or (x+1) % 5  == 0 ]) 
+    return sum([x for x in range(number) if x % 3  == 0 or x % 5  == 0 ]) 
