0d250e9f9bba4e7dc7271c05176e31d2546db679
Timewound <rubenjms1@gmail.com>
2024-01-12 10:10:23 -0300

    8 kyu

---
+++ python/leonardo_oscar.py
@@ -0,0 +1,7 @@
+#https://www.codewars.com/kata/56d49587df52101de70011e4
+def leo(oscar):
+    if oscar == 88: return  "Leo finally won the oscar! Leo is happy"
+    if oscar == 86: return "Not even for Wolf of wallstreet?!"
+    if oscar > 88: return "Leo got one already!"
+    
+    return "When will you give Leo an Oscar?"
