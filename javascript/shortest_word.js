a5bc5f425e7939a42011672d202e69e11cb2373a
Timewound <rubenjms1@gmail.com>
2024-02-12 22:57:59 -0300

    7 kyu

--- javascript/shortest_word.js
+++ javascript/shortest_word.js
@@ -1,8 +1,8 @@
 //https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
 function findShort(s){
 
-    var words = s.split(" ")
-    var words_len = words.map(x => 
+    let words = s.split(" ")
+    let words_len = words.map(x => 
         x.length)
 
     return Math.min.apply(Math, words_len)
