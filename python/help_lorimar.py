218267b04cf98033349b2004ab75ccd05a96bfd9
Timewound <rubenjms1@gmail.com>
2024-02-14 10:51:59 -0300

    7 kyu

---
+++ python/help_lorimar.py
@@ -0,0 +1,11 @@
+#https://www.codewars.com/kata/57102bbfd860a3369300089c
+from math import sqrt
+def sensor_analysis(sensor_data):
+    datas = [data[1] for data in sensor_data]
+
+    mean = round(sum(datas) / len(sensor_data), 4)
+    print([(data-mean)**2 for data in datas])
+    standard_deviation = sqrt(sum([(data-mean)**2 for data in datas]) / (len(sensor_data)-1))
+    
+    return mean, round(standard_deviation, 4)
+  
