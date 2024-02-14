cfedae5841ef39e267147748d722ebea7a335c49
Timewound <rubenjms1@gmail.com>
2024-02-14 10:52:41 -0300

    7 kyu - removendo print()

--- python/help_lorimar.py
+++ python/help_lorimar.py
@@ -4,7 +4,7 @@ def sensor_analysis(sensor_data):
     datas = [data[1] for data in sensor_data]
 
     mean = round(sum(datas) / len(sensor_data), 4)
-    print([(data-mean)**2 for data in datas])
+    
     standard_deviation = sqrt(sum([(data-mean)**2 for data in datas]) / (len(sensor_data)-1))
     
     return mean, round(standard_deviation, 4)
