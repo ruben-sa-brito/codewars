b9723806dc4cc8cfcc5190b3b23a988fcdc8ec1d
Ruben Sa <rubenjms1@gmail.com>
2024-08-15 09:05:49 -0300

    kata - Calculating with Functions

---
+++ python/5 kyu/calculating_function.py
@@ -0,0 +1,29 @@
+def zero(func=''): 
+    return eval("0" + func)
+def one(func=''): 
+    return eval("1" + func) 
+def two(func=''):
+    return eval("2" + func)
+def three(func=''):
+    return eval("3" + func)
+def four(func=''): 
+    return eval("4" + func) 
+def five(func=''): 
+    return eval("5" + func) 
+def six(func=''):
+    return eval("6" + func) 
+def seven(func=''):
+    return eval("7" + func)
+def eight(func=''): 
+    return eval("8" + func)
+def nine(func=''):
+    return eval("9" + func)
+
+def plus(number): 
+    return f"+{number}"
+def minus(number): 
+    return f"-{number}"
+def times(number): 
+    return f"*{number}"
+def divided_by(number): 
+    return f"//{number}"
